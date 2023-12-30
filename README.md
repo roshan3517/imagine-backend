095d7d0b1cac0f098d86f732e2e728e11876a2b7
roshan3517 <tanpureroshan3517@gmail.com>
2023-12-30 13:43:49 +0530

    suceesfully

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# imagine-backend
-imagine-backend
+# backend
---
+++ model/Comment.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Story = require("./Story");
+const Reply = require("./Reply");
+
+const CommentSchema = new Schema({
+  commenter: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    default: new Date().toLocaleDateString(),
+    required: true,
+  },
+  time: {
+    type: String,
+    default: new Date().toLocaleTimeString(),
+    required: true,
+  },
+  edited: {
+    type: Boolean,
+    default: false,
+  },
+  story: {
+    type: Schema.Types.ObjectId,
+    ref: "Story",
+  },
+  replies: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Reply",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
